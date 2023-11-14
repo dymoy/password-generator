31f02f1f7521a9930c01409f872bceceb9b981c3
Derek Moy <derekymoy@gmail.com>
2023-11-13 20:20:46 -0800

    criteria pop up displays when clicking Generate password. TODO: store form checkbox values.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,11 +3,31 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  getCriteria();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+// Gets user input for password criteria and returns a string
+function getCriteria() {
+  document.querySelector(".criteria-popup-container").style.display = "flex";
+}
+
+function getCheckBoxValues(form) {
+  console.log('entered');
+  let values = [];
+  let checkboxes = form.criteria;
+
+  for (let i = 0, formLength = checkboxes.length; i < formLength; i++) {
+    if (checkboxes[i].checked) {
+      values.push(checkboxes[i].value);
+    }
+  }
+  console.log(values)
+  document.querySelector(".criteria-popup-container").style.display = "none";
 }
 
 // Add event listener to generate button
